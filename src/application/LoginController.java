1d1e262a1deda3c844479b010f187bd4a595b7c2
dsimog01 <dsimog01@dsimog01PC>
2020-12-06 21:10:13 +0100

    Implementado método de ejemplo login()

---
+++ src/application/LoginController.java
@@ -0,0 +1,40 @@
+package application;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+
+public class LoginController {
+	
+	@FXML
+	private TextField textoUsuario;
+	
+	@FXML 
+	private TextField textoContrasena;
+	
+	
+	public void login() {
+		
+		String usuario, contrasena;
+		
+		usuario = textoUsuario.getText();
+		contrasena = textoContrasena.getText();
+		
+		if(comprobarCredencialesOK(usuario, contrasena)) {
+			//loginOK
+		}else {
+			//throw new WrongPassWordException;
+		}
+		
+		
+		System.out.println("Comprobando usuario:" + usuario + contrasena);
+	}
+	
+	private boolean comprobarCredencialesOK(String usuario, String contrasena) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	public void registrar() {
+		
+	}
+}
